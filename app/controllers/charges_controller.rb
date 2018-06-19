fa76cf6d2dfab996b6b641f51347e5a2ef09abb1
rola <rola.elbakly@gmail.com>
2018-06-19 14:10:54 +0200

    Connected account done

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -5,23 +5,25 @@ class ChargesController < ApplicationController
     def create
       @app_fee= params[:amount] * 0.05
       
-      
+
+      Stripe.api_key = "sk_test_mXLOIO7kwjYPpg80ckqs8VSF"
+
       customer = Stripe::Customer.create(
         :email => params[:stripeEmail],
         :source  => params[:stripeToken]
       )
-      token = Stripe::Token.create({
-             :customer => customer.id,
-            }, {:stripe_account => params[:account_id]})
+      #token = Stripe::Token.create({
+          #  :customer => customer.id,
+          #  }, {:stripe_account => params[:account_id]})
 
-    
+            
       charge = Stripe::Charge.create({
 
         :amount      => params[:amount],
         :description => 'Rails Stripe customer',
         :currency    => 'usd',
-        :card     => token.id,
-        :application_fee => 200,
+        :source       =>"tok_visa",
+        :application_fee => params[:amount],
     }, :stripe_account => params[:account_id])
     
     rescue Stripe::CardError => e
